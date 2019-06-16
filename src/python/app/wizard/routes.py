81139c11afcc7b718b47e2c97ecbdc1f2f609252
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-06-16 17:38:17 +0200

    registration step done

--- src/python/app/wizard/routes.py
+++ src/python/app/wizard/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, flash, redirect, url_for, current_app
 import psycopg2
+from psycopg2 import sql
 import uuid
+import bcrypt
 
 from app.wizard import wizard as wiz
 
@@ -12,52 +14,56 @@ def wizard():
 @wiz.route("/process-registration", methods=["POST"])
 def registration_processing():
     config = current_app.config
+    filled = {}
+    filled['username'] = request.form.get('username')
+    filled["display_name"] = request.form.get("display_name")
+    filled["password"] = request.form.get("password")
+    filled["email"] = request.form.get("email")
+    filled["sitename"] = request.form.get("sitename")
 
-    filled = {
-        "username": request.values["username"],
-        "display_name": request.values["display_name"],
-        "password": request.values["password"],
-        "email": request.values["email"],
-        "sitename": request.values["sitename"]
-        }
+    print(filled)
 
     con = psycopg2.connect("dbname='"+config["DATABASE_NAME"]+"' user='"+config["DATABASE_USER"]+"' host='"+config["DATABASE_URL"]+"' password='"+config["DATABASE_PASSWORD"]+"'")
 
     cur = con.cursor()
 
     cur.execute(
-            sql.SQL("SELECT * FROM sloth_users WHERE username = \"{}\"")
-               .format(sql.Identifier(filled.username))
+            sql.SQL("SELECT * FROM sloth_users WHERE username = '{}'")
+               .format(sql.Identifier(filled['username']))
         )
     items = cur.fetchall()
-
+    
     if (len(items) == 0):        
-        user = {
-            "uuid": uuid.uuid4(),
-            "username": filled.username,
-            "display_name": filled.display_name,
-            "password": bcrypt.hashpw(filled.password.encode("utf-8"), bcrypt.gensalt(rounds=15)),
-            "email": filled.email 
-        }
+        user = {}
+        user["uuid"] = str(uuid.uuid4())
+        user["username"] = filled["username"]
+        user["password"] = bcrypt.hashpw(filled["password"].encode("utf-8"), bcrypt.gensalt(rounds=15)).decode("utf-8")
+        user["email"] = filled["email"]
+        print(user)
         cur.execute(
-            sql.SQL("INSERT INTO sloth_users(uuid, username, display_name, password, email) VALUES (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\")")
-               .format(sql.Identifier(user.uuid),
-                       sql.Identifier(user.username),
-                       sql.Identifier(user.display_name),
-                       sql.Identifier(user.password),
-                       sql.Identifier(user.email))
+            sql.SQL("INSERT INTO sloth_users(uuid, username, display_name, password, email) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')")
+               .format(sql.Identifier(user["uuid"]),
+                       sql.Identifier(user["username"]),
+                       sql.Identifier(user["username"]),
+                       sql.Identifier(user["password"]),
+                       sql.Identifier(user["email"]))
         )
         cur.execute(
-            sql.SQL("INSERT INTO sloth_settings(settings_name, display_name, settings_value, section_id, section_type, section_name) VALUES (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\")")
-               .format(sql.Identifier(filled.sitename))
+            sql.SQL("INSERT INTO sloth_settings VALUES ('sitename', 'Sitename', '{0}', '0', 'parent', 'Settings')")
+               .format(sql.Identifier(filled["sitename"]))
         )
         con.commit()
+        cur.close()
+        con.close()
 
+        return redirect("/registered")
+    
     cur.close()
     con.close()
-
+    
+    filled['password'] = ""
     return render_template("initial_step.html", filled=filled)
 
 @wiz.route("/registered", methods=["GET"])
 def registered():
-    return ""
+    return render_template("final_step.html")
