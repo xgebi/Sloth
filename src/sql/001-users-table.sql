900d541050c3710c3bad6f2298171cff2d9a43dd
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-07-06 09:24:10 +0200

    added posts sql, adjustments to users

--- src/sql/001-users-table.sql
+++ src/sql/001-users-table.sql
@@ -1,9 +1,13 @@
 CREATE TABLE sloth_users (
-    uuid                VARCHAR(200) PRIMARY KEY,
-    username            VARCHAR(50) UNIQUE,
-    display_name        VARCHAR(100),
-    password            VARCHAR(500),
-    email               VARCHAR(350),
-    token               VARCHAR(350),
-    expiry_date         BIGINT
+    uuid character varying(200) NOT NULL PRIMARY KEY,
+    username character varying(50) UNIQUE,
+    display_name character varying(100),
+    password character varying(500),
+    email character varying(350),
+    token character varying(350),
+    expiry_date double precision,
+    permissions_level integer DEFAULT 0 NOT NULL
 );
+
+
+ALTER TABLE sloth_users OWNER TO sloth;
---
+++ src/sql/003-posts.sql
@@ -0,0 +1,15 @@
+CREATE TABLE sloth_posts (
+    uuid character varying(200) NOT NULL PRIMARY KEY,
+    slug character varying(200) UNIQUE,
+    title character varying(220),
+    content text,
+    css_file character varying(220),
+    js_file character varying(220),
+    publish_date double precision,
+    update_date double precision,
+    post_status character  varying(10),
+    tags text[],
+    categories text[]
+);
+
+ALTER TABLE sloth_posts OWNER TO sloth;
