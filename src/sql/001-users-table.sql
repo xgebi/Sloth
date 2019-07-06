f83ce72b8be8c3eed1436c6c21b60b71ae54aee8
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-07-06 10:03:26 +0200

    spaces to tabs, part 2

--- src/sql/001-users-table.sql
+++ src/sql/001-users-table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE sloth_users (
-    uuid character varying(200) NOT NULL PRIMARY KEY,
-    username character varying(50) UNIQUE,
-    display_name character varying(100),
-    password character varying(500),
-    email character varying(350),
-    token character varying(350),
-    expiry_date double precision,
-    permissions_level integer DEFAULT 0 NOT NULL
+	uuid character varying(200) NOT NULL PRIMARY KEY,
+	username character varying(50) UNIQUE,
+	display_name character varying(100),
+	password character varying(500),
+	email character varying(350),
+	token character varying(350),
+	expiry_date double precision,
+	permissions_level integer DEFAULT 0 NOT NULL
 );
 
 
