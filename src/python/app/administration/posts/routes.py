8179743016693047b1e9c5c67f39fe2718d4d001
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-07-06 21:22:47 +0200

    post type template

--- src/python/app/administration/posts/routes.py
+++ src/python/app/administration/posts/routes.py
@@ -11,5 +11,29 @@ def list_posts(uuid):
 	return render_template("list.html")
 
 @posts.route("/post_type/<uuid>/add")
-def add_post(uuid):
+def add_post_screen(uuid):
 	return render_template("new_post.html")
+
+@posts.route("/post_type/<uuid>/create")
+def create_new_post(uuid):
+	pass
+
+@posts.route("/post_types")
+def redirect_to_post_type_list(uuid):
+	return redirect(f"/post_types/{uuid}/list")
+
+@posts.route("/post_types/list")
+def list_all_post_types(uuid):
+	pass
+
+@posts.route("/post_types/<uuid>")
+def show_post_type_details(uuid):
+	pass
+
+@posts.route("/post_types/<uuid>/categories")
+def show_post_type_categories(uuid):
+	pass
+
+@posts.route("/post_types/<uuid>/tags")
+def show_post_type_tags(uuid):
+	pass
