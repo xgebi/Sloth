8d252b9babeba88e1b13d0cd443718b7037d96c8
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-07-06 20:20:22 +0200

    post types are being listed

--- src/python/app/administration/dashboard/routes.py
+++ src/python/app/administration/dashboard/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, flash, redirect, url_for, current_app
-
+import psycopg2
 
 from app.administration.dashboard import dashboard
 from app.authorization.user import User
+from app.posts.post_types import PostTypes
 
 @dashboard.route("/dashboard", methods=["GET"])
 def dashboard():
@@ -10,5 +11,15 @@ def dashboard():
 	userToken = request.cookies.get('userToken')
 	user = User(userId, userToken)
 	if user.authorize_user(0):
-		return render_template("dashboard.html")
+		config = current_app.config
+		con = psycopg2.connect("dbname='"+config["DATABASE_NAME"]+"' user='"+config["DATABASE_USER"]+"' host='"+config["DATABASE_URL"]+"' password='"+config["DATABASE_PASSWORD"]+"'")
+		cur = con.cursor()
+		post_types = PostTypes.get_post_type_list(None, con)
+
+
+		cur.close()
+		con.close()
+		
+		return render_template("dashboard.html", post_types = post_types)
+	
 	return redirect("/login")
--- src/python/app/posts/post_types.py
+++ src/python/app/posts/post_types.py
@@ -1,8 +1,18 @@
+import psycopg2
+
 class PostTypes:
 	def __init__(self):
 		pass
 		# Placeholder for the future
 
-	def get_post_type_list(self):
-		pass
-		# TODO Get list of post types
+	def get_post_type_list(self, connection):
+		cur = connection.cursor()
+		raw_items = []
+		try:
+			cur.execute(
+				"SELECT uuid, display_name FROM sloth_post_types"
+			)
+			raw_items = cur.fetchall()
+		except Exception as e:
+			abort(500)
+		return raw_items
