9b19322de4b4f0222b60c3566d1363ecda4c4046
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-07-14 15:14:19 +0200

    Listing posts works although  it's not pretty yet

--- src/python/app/administration/posts/routes.py
+++ src/python/app/administration/posts/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, request, flash, redirect, url_for, current_app
+from flask import render_template, request, flash, redirect, url_for, current_app, abort
+import psycopg2
 
 from app.administration.posts import posts
+from app.authorization.user import User
+from app.administration.posts.post_types import PostTypes
 
 # Posts
 @posts.route("/post_type/<uuid>")
@@ -9,7 +12,32 @@ def redirect_to_list(uuid):
 
 @posts.route("/post_type/<uuid>/list")
 def list_posts(uuid):
-	return render_template("list.html")
+	userId = request.cookies.get('userID')
+	userToken = request.cookies.get('userToken')
+	user = User(userId, userToken)
+	if user.authorize_user(0):
+		config = current_app.config
+		con = psycopg2.connect("dbname='"+config["DATABASE_NAME"]+"' user='"+config["DATABASE_USER"]+"' host='"+config["DATABASE_URL"]+"' password='"+config["DATABASE_PASSWORD"]+"'")
+		cur = con.cursor()
+		post_types = PostTypes.get_post_type_list(None, con)
+
+		try:
+			cur.execute(
+				"SELECT uuid, title, publish_date, update_date, post_status, tags, categories FROM sloth_posts WHERE post_type = %s",
+				[uuid]
+			)
+			raw_items = cur.fetchall()
+		except Exception as e:
+			print(e)
+			abort(500)
+
+
+		cur.close()
+		con.close()
+		
+		return render_template("list.html", post_types = post_types, posts = raw_items, post_type = uuid)
+	
+	return redirect("/login")
 
 @posts.route("/post_type/<uuid>/add")
 def add_post_screen(uuid):
@@ -18,6 +46,10 @@ def add_post_screen(uuid):
 @posts.route("/post_type/<uuid>/create")
 def create_new_post(uuid):
 	pass
+
+@posts.route("/post_type/<uuid>/edit/<post_uuid>")
+def edit_post(uuid, post_uuid):
+	return "Hello and bye"
 
 # Post types
 @posts.route("/post_types")
@@ -32,10 +64,22 @@ def list_all_post_types(uuid):
 def show_post_type_details(uuid):
 	pass
 
+@posts.route("/post_types/<uuid>/save")
+def save_post_type_details(uuid):
+	pass
+
 @posts.route("/post_types/<uuid>/categories")
 def show_post_type_categories(uuid):
 	pass
 
+@posts.route("/post_types/<uuid>/categories/save")
+def save_post_type_categories(uuid):
+	pass
+
 @posts.route("/post_types/<uuid>/tags")
 def show_post_type_tags(uuid):
+	pass
+
+@posts.route("/post_types/<uuid>/tags/save")
+def save_post_type_tags(uuid):
 	pass
