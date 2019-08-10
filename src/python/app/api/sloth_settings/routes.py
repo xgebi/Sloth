ddda92c6e16feb31bcf1eb43426d31b107c29c72
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-08-10 22:52:48 +0200

    Saving settings done

--- src/python/app/api/sloth_settings/routes.py
+++ src/python/app/api/sloth_settings/routes.py
@@ -39,7 +39,47 @@ def show_settings(*args, connection=None, **kwargs):
 		items.append({
 			"settingsName": item[0],
 			"displayName": item[1],
-			"settingsValue": item[2]
+			"settingsValue": item[2],
+			"settingsValueType": item[3]
 		})
 
 	return json.dumps({ "postTypes": postTypesResult, "settings": items })
+
+@sloth_settings.route("/api/settings/save", methods=["PUT", "POST"])
+@authorize(1)
+@db_connection
+def save_settings(*args, connection=None, **kwargs):
+	updated_settings = json.loads(request.data);
+	print(updated_settings)
+
+	cur = connection.cursor()
+	raw_items = []
+	try:		
+		for setting in updated_settings['settings']:
+			cur.execute(
+				sql.SQL("UPDATE sloth_settings SET settings_value = %s WHERE settings_name = %s"),
+				[setting["settingsValue"], setting["settingsName"]]
+			)
+		connection.commit()
+
+		cur.execute(
+			"SELECT settings_name, display_name, settings_value, settings_value_type FROM sloth_settings WHERE settings_type = 'sloth'"
+		)
+		raw_items = cur.fetchall()
+	except Exception as e:
+		print("db error")
+		abort(500)
+
+	cur.close()
+	connection.close()
+
+	items = []
+	for item in raw_items:
+		items.append({
+			"settingsName": item[0],
+			"displayName": item[1],
+			"settingsValue": item[2],
+			"settingsValueType": item[3]
+		})
+
+	return json.dumps({ "settings": items })
