1c1cfe2da449e13954e4bf7168216c1697fb82cf
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-11-19 08:32:34 +0100

    Sidebar items

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slothcms",
-    version="0.0.109",
+    version="0.0.110",
     author="Sarah Gebauer",
     author_email="sarah@sarahgebauer.com",
     description="Sloth Content Management System",
--- src/cms/app/web/dashboard/routes.py
+++ src/cms/app/web/dashboard/routes.py
@@ -1,6 +1,8 @@
 from flask import request, flash, url_for, current_app, abort, redirect
 from app.utilities.db_connection import db_connection
 from app.authorization.authorize import authorize_web
+from app.posts.post_types import PostTypes
+from app.utilities.db_connection import db_connection
 
 from toes.toes import render_toe
 
@@ -10,5 +12,8 @@ from app.web.dashboard import dashboard
 
 @dashboard.route('/dashboard')
 @authorize_web(0)
-def show_dashboard():
-   return render_toe(template="dashboard.toe", path_to_templates=current_app.config["TEMPLATES_PATH"])
+@db_connection
+def show_dashboard(*args, connection, **kwargs):
+   postTypes = PostTypes()
+   postTypesResult = postTypes.get_post_type_list(connection)
+   return render_toe(template="dashboard.toe", path_to_templates=current_app.config["TEMPLATES_PATH"], data={ "post_types": postTypesResult })
