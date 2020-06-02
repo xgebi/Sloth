a0a9f0237069cf446b1b55a4b67474747275fd20
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-06-02 23:03:21 +0200

    upload file

--- src/cms/app/api/post/routes.py
+++ src/cms/app/api/post/routes.py
@@ -21,10 +21,7 @@ reserved_folder_names = ('tag', 'category')
 @post.route("/api/post/media", methods=["GET"])
 @authorize_rest(0)
 @db_connection
-def show_post_new(*args, permission_level, connection, post_type, **kwargs):	
-	postTypes = PostTypes()
-	postTypesResult = postTypes.get_post_type_list(connection)
-
+def show_post_new(*args, connection, **kwargs):	
 	cur = connection.cursor()
 	media = []
 	try:
@@ -33,11 +30,6 @@ def show_post_new(*args, permission_level, connection, post_type, **kwargs):
 			sql.SQL("SELECT uuid, file_path, alt FROM sloth_media")
 		)	
 		media = cur.fetchall()
-		cur.execute(
-			sql.SQL("SELECT display_name FROM sloth_post_types WHERE uuid = %s"),
-			[post_type]
-		)
-		post_type_name = cur.fetchone()[0]
 	except Exception as e:
 		print("db error")
 		abort(500)
@@ -45,7 +37,7 @@ def show_post_new(*args, permission_level, connection, post_type, **kwargs):
 	cur.close()
 	connection.close()
 	
-	return json.dumps({})
+	return json.dumps({"media": media})
 
 
 @post.route("/api/post/upload-file", methods=['POST'])
@@ -58,22 +50,20 @@ def upload_image(*args, file_name, connection=None, **kwargs):
 	with open(os.path.join(current_app.config["OUTPUT_PATH"], "sloth-content", file_name), 'wb') as f:
 		f.write(request.data)
 
-	url = ""
+	file = {}
 
 	try:
 		cur = connection.cursor()
+	
+		# sql.SQL("SELECT uuid, file_path, alt FROM sloth_media")
 		cur.execute(
-			sql.SQL("SELECT settings_value FROM sloth_settings WHERE settings_name = %s"), ["site_url"]
+			sql.SQL("INSERT INTO settings_media VALUES (%s, %s, %s, %s) RETURNING uuid, file_path, alt"), [str(uuid.uuid4()), os.path.join(current_app.config["OUTPUT_PATH"], "sloth-content", file_name), "", ""]
 		)
-		url = cur.fetchone()
+		file = cur.fetchone()
 		cur.close()
 	except Exception as e:
 		print(traceback.format_exc())
 		connection.close()
 		abort(500)
-	
-	temp_files = [f for f in os.listdir(os.path.join(current_app.config["OUTPUT_PATH"], "sloth-content")) if os.path.isfile(os.path.join(current_app.config["OUTPUT_PATH"], "sloth-content", f)) and f[f.rfind(".") + 1:].lower() in ("jpg", "jpeg", "png", "svg", "bmp", "tiff")]
 
-	files = [url[0] + "/sloth-content/" + f for f in temp_files]
-
-	return json.dumps({ "galleryList": files }), 201
+	return json.dumps({ "media": file }), 201
