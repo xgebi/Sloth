af30adb529721eed73a10ddf291b17dce940a90c
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-08-30 21:11:06 +0200

    Have to make up my mind about refactoring 🎑

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slothcms",
-    version="0.1.0",
+    version="0.1.1",
     author="Sarah Gebauer",
     author_email="sarah@sarahgebauer.com",
     description="Sloth Content Management System",
