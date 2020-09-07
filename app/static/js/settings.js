cd2806dec19275a84711e5408069a894fa600165
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-09-07 21:57:33 +0200

    Manual unlocking 🔓

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // 1. open upload modal
     document.querySelector("#delete-button").addEventListener('click', deleteDbContent);
+    document.querySelector("#unlock-generation-button").addEventListener('click', deleteGenerationLock);
     // 2. upload file
 
     // 3. delete file query
@@ -8,6 +9,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function deleteDbContent() {
     fetch('/api/content/clear', {
+        method: 'DELETE',
+        headers: {
+            'authorization': document.cookie
+                .split(';')
+                .find(row => row.trim().startsWith('sloth_session'))
+                .split('=')[1],
+        }
+    }).then(response => {
+        console.log(response);
+        return response.json()
+    }).then(data => {
+        console.log('Success:', data);
+    }).catch((error) => {
+        console.error('Error:', error);
+    })
+}
+
+function deleteGenerationLock() {
+    fetch('/api/settings/generation-lock', {
         method: 'DELETE',
         headers: {
             'authorization': document.cookie
