9f3b2b32209b1f54d3cb318299dce8f1a7b2a093
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-09-09 22:52:24 +0200

    Mock Endpoints ☢

--- app/mock_endpoints/routes.py
+++ app/mock_endpoints/routes.py
@@ -1,6 +1,8 @@
-from flask import request, abort, redirect, render_template
+from flask import request, abort, redirect, render_template, make_response, current_app
+from flask_cors import cross_origin
 from psycopg2 import sql
 import json
+import uuid
 from app.post.post_types import PostTypes
 from app.utilities.db_connection import db_connection
 from app.authorization.authorize import authorize_web, authorize_rest
@@ -12,37 +14,182 @@ from app.mock_endpoints import mock_endpoints
 @authorize_web(0)
 @db_connection
 def show_endpoints_list(*args, permission_level, connection, **kwargs):
+    if connection is None:
+        abort(500)
     post_types = PostTypes()
     post_types_result = post_types.get_post_type_list(connection)
 
-    return render_template("mock-endpoints-list.html", post_types=post_types_result, permission_level=permission_level
+    cur = connection.cursor()
+    temp_endpoints_list = []
+    try:
+        cur.execute(
+            sql.SQL("""SELECT uuid, path FROM sloth_mock_endpoints ORDER BY path DESC;""")
+        )
+        temp_endpoints_list = cur.fetchall()
+    except Exception as e:
+        print(e)
+        abort(500)
+
+    cur.close()
+    connection.close()
+    endpoints_list = []
+    for endpoint in temp_endpoints_list:
+        endpoints_list.append({
+            "uuid": endpoint[0],
+            "path": endpoint[1]
+        })
+
+    return render_template(
+        "mock-endpoints-list.html",
+        post_types=post_types_result,
+        permission_level=permission_level,
+        endpoints_list=endpoints_list
                            )
 
 
 @mock_endpoints.route("/mock-endpoints/<endpoint>", methods=["GET"])
 @authorize_web(0)
 @db_connection
-def show_endpoint(*args, permission_level, connection, **kwargs):
+def show_endpoint(*args, permission_level, connection, endpoint, **kwargs):
+    if connection is None:
+        abort(500)
     post_types = PostTypes()
     post_types_result = post_types.get_post_type_list(connection)
 
-    return render_template("mock-endpoint.html", post_types=post_types_result, permission_level=permission_level
+    cur = connection.cursor()
+    temp_endpoint_result = {}
+    try:
+        cur.execute(
+            sql.SQL("""SELECT uuid, path, data FROM sloth_mock_endpoints WHERE uuid = %s"""),
+            [endpoint]
+        )
+        temp_endpoint_result = cur.fetchone()
+    except Exception as e:
+        print(e)
+        abort(500)
+
+    cur.close()
+    connection.close()
+
+    endpoint_result = {
+        "uuid": temp_endpoint_result[0],
+        "path": temp_endpoint_result[1],
+        "data": temp_endpoint_result[2],
+        "new": False
+    }
+
+    return render_template(
+        "mock-endpoint.html",
+        post_types=post_types_result,
+        permission_level=permission_level,
+        endpoint=endpoint_result
                            )
+
+
+@mock_endpoints.route("/api/mock-endpoints/<endpoint>/delete", methods=["DELETE"])
+@authorize_web(0)
+@db_connection
+def delete_endpoint(*args, permission_level, connection, endpoint, **kwargs):
+    if connection is None:
+        abort(500)
+
+    cur = connection.cursor()
+    try:
+        cur.execute(
+            sql.SQL("""DELETE FROM sloth_mock_endpoints WHERE uuid = %s"""),
+            [endpoint]
+        )
+        connection.commit()
+    except Exception as e:
+        print(e)
+        abort(500)
+
+    cur.close()
+    connection.close()
+
+    return json.dumps({"endpoint": "deleted"}), 204
 
 
 @mock_endpoints.route("/mock-endpoints/new", methods=["GET"])
 @authorize_web(0)
 @db_connection
-def show_endpoint(*args, permission_level, connection, **kwargs):
+def show_new_endpoint(*args, permission_level, connection, **kwargs):
+    if connection is None:
+        abort(500)
     post_types = PostTypes()
     post_types_result = post_types.get_post_type_list(connection)
 
-    return render_template("mock-endpoint.html", post_types=post_types_result, permission_level=permission_level
+    return render_template(
+        "mock-endpoint.html",
+        post_types=post_types_result,
+        permission_level=permission_level,
+        endpoint={"uuid": str(uuid.uuid4()), "new": True}
                            )
 
 
-@mock_endpoints.route("/mock-endpoints", methods=["POST", "PUT"])
+@mock_endpoints.route("/mock-endpoints/<endpoint_id>/save", methods=["POST", "PUT"])
 @authorize_web(0)
 @db_connection
-def save_endpoint(*args, permission_level, connection, **kwargs):
-    pass
+def save_endpoint(*args, permission_level, connection, endpoint_id, **kwargs):
+    if connection is None:
+        abort(500)
+
+    filled = request.form
+    for key in filled.keys():
+        if len(filled[key]) == 0:
+            abort(400)
+
+    cur = connection.cursor()
+    try:
+        if filled["new"].lower() == "true":
+            cur.execute(
+                sql.SQL("""INSERT INTO sloth_mock_endpoints VALUES (%s, %s, %s)"""),
+                [endpoint_id, filled["path"], filled["data"]]
+            )
+        else:
+            cur.execute(
+                sql.SQL("""UPDATE sloth_mock_endpoints SET path = %s, data = %s WHERE uuid = %s"""),
+                [filled["path"], filled["data"], endpoint_id]
+            )
+        connection.commit()
+    except Exception as e:
+        abort(500)
+
+    cur.close()
+    connection.close()
+
+    return redirect(f"/mock-endpoints/{endpoint_id}")
+
+
+@mock_endpoints.route("/api/mock/<path>", methods=["GET"])
+@cross_origin()
+@db_connection
+def get_endpoint(*args, connection, path, **kwargs):
+    if request.host not in current_app.config["ALLOWED_REQUEST_HOSTS"]:
+        abort(500)
+    if connection is None:
+        abort(500)
+
+    result = ""
+    cur = connection.cursor()
+    try:
+        cur.execute(
+            sql.SQL("""SELECT data FROM sloth_mock_endpoints WHERE path = %s;"""),
+            [path]
+        )
+        temp_result = cur.fetchone()
+        if temp_result is not None and len(temp_result) == 1:
+            result = temp_result[0]
+        else:
+            result = json.dumps({"error": "Missing data"})
+    except Exception as e:
+        print(e)
+        abort(500)
+
+    cur.close()
+    connection.close()
+
+    response = make_response(result)
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
