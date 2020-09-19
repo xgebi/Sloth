020d4db3bdc3a1a93644d8b0480e7f97f6667d3f
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-09-19 23:12:45 +0200

    WIP Toes Node 🚧

---
+++ app/toes/Node.py
@@ -0,0 +1,42 @@
+from typing import Dict, Tuple, Sequence, List, Type
+
+
+class Node:
+
+    def __init__(
+            self,
+            *args,
+            name: str,
+            attributes: Dict = {},
+            children: List = [],
+            paired_tag: bool = True,
+            **kwargs
+    ) -> None:
+
+        self.name = name
+        if type(attributes) is set:
+            self.attributes = {}
+            for attr in attributes:
+                self.attributes[attr] = ""
+        else:
+            self.attributes = attributes
+        if paired_tag:
+            self.children = children
+        self.paired_tag = paired_tag
+
+    def set_attribute(self, name: str, value: str = ""):
+        self.attributes[name] = value
+
+    def remove_attribute(self, name: str):
+        del self.attributes[name]
+
+    def add_child(self, child):
+        if self.paired_tag:
+            self.children.append(child)
+
+    def remove_child(self, child):
+        if self.paired_tag and child in self.children:
+            self.children.remove(child)
+
+    def to_xml_string(self):
+        pass
