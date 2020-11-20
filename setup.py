677a7ef02044bd99148d681d6ce30d0d7d9feaa2
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-11-20 22:02:38 +0100

    bumped version to 0.3.1 😘

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slothcms",
-    version="0.3.0",
+    version="0.3.1",
     author="Sarah Gebauer",
     author_email="sarah@sarahgebauer.com",
     description="Sloth Content Management System",
