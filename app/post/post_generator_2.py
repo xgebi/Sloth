70a7f63559823cfa9ca337fb8f9d7eb8f37b1945
Sarah Gebauer <xgebi@users.noreply.github.com>
2020-12-02 22:12:27 +0100

    Working on PostGenerator v2️⃣

--- app/post/post_generator_2.py
+++ app/post/post_generator_2.py
@@ -4,25 +4,72 @@ from pathlib import Path
 import os
 from jinja2 import Template
 import json
+import threading
 
 from app.utilities.db_connection import db_connection
 from app.toes.markdown_parser import MarkdownParser
 
 
 class PostGenerator:
-    runnable = False
+    runnable = True
     theme_path = ""
     menus = {}
 
     @db_connection
     def __init__(self, *args, connection, **kwargs):
         if connection is None:
-            return
+            self.is_runnable = False
 
         self.connection = connection
         self.config = current_app.config
         self.theme_path = self.get_theme_path()
         self.menus = self.get_menus()
+
+        self.set_individual_settings(connection=connection, setting_name='active_theme')
+        self.set_individual_settings(connection=connection, setting_name='main_language')
+
+        # Set path to the theme
+        self.theme_path = Path(
+            self.config["THEMES_PATH"],
+            self.settings['active_theme']['settings_value']
+        )
+        self.set_footer(connection=connection)
+
+    def run(self, *args, lang: str, post: str, post_type: str, everything: bool = True, **kwargs):
+        """ Main function that runs everything
+
+            Attributes
+            ----------
+            lang : str
+                UUID of a language, defaults to main language
+            post: str
+                Everything regarding a post will be regenerated
+            post_type : str
+                UUID of a post type, can be empty
+            everything : bool
+                Tells the function that everything will be regeneratable
+        """
+        if not self.runnable or (post and post_type):
+            return False
+
+        if Path(os.path.join(os.getcwd(), 'generating.lock')).is_file():
+            # return self.add_to_queue(post=post)
+
+        with open(os.path.join(os.getcwd(), 'generating.lock'), 'w') as f:
+            f.write("generation locked")
+
+        if post:
+            pass
+        elif post_type:
+            pass
+        elif everything:
+            t = threading.Thread(target=self.generate_all)
+        else:
+            os.remove(Path(os.path.join(os.getcwd(), 'generating.lock')))
+            return False
+        t.start()
+        return True
+
 
     def get_theme_path(self, *args, **kwargs):
         cur = self.connection.cursor()
@@ -66,7 +113,7 @@ class PostGenerator:
         cur.close()
         return menus
 
-    def get_protected_post(self, *args, uuid,**kwargs):
+    def get_protected_post(self, *args, uuid, **kwargs):
         post = {}
         try:
             cur = self.connection.cursor()
@@ -100,3 +147,49 @@ class PostGenerator:
 
         else:
             return post
+
+    def set_footer(self, *args, connection, **kwargs):
+        # Footer for post
+        raw_api_url = []
+        cur = connection.cursor()
+        try:
+            cur.execute(
+                sql.SQL("""SELECT settings_value FROM sloth_settings 
+                        WHERE settings_name = %s"""),
+                ['api_url']
+            )
+            raw_api_url = cur.fetchone()
+        except Exception as e:
+            print(traceback.format_exc())
+        cur.close()
+
+        with open(Path(__file__).parent / "../templates/analytics.html", 'r') as f:
+            footer_template = Template(f.read())
+            cur = connection.cursor()
+            raw_item = []
+
+            if len(raw_api_url) == 1:
+                self.sloth_footer = footer_template.render(api_url=raw_api_url[0])
+            else:
+                self.sloth_footer = ""
+
+    def set_individual_settings(self, *args, connection, setting_name, **kwargs):
+        cur = connection.cursor()
+        try:
+            cur.execute(
+                sql.SQL("""SELECT settings_name, settings_value, settings_value_type 
+                                FROM sloth_settings WHERE settings_name = %s OR settings_type = %s"""),
+                [setting_name, 'sloth']
+            )
+            raw_items = cur.fetchall()
+        except Exception as e:
+            print(e)
+
+        cur.close()
+
+        for item in raw_items:
+            self.settings[str(item[0])] = {
+                "settings_name": item[0],
+                "settings_value": item[1],
+                "settings_value_type": item[2]
+            }
