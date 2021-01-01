182477cd63983cb33593e3bf37f2b8ebf3a85ed7
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-01-01 17:13:43 +0100

    WIP post formats 🌓

--- app/static/js/post-formats.js
+++ app/static/js/post-formats.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector("#create-new-post-format").addEventListener('click', addPostFormat);
+    document.querySelectorAll(".edit-button").forEach(button => {
+        button.addEventListener('click', editRow)
+    })
 
 });
+
+function addPostFormat() {
+
+}
+
+function editRow() {
+
+}
