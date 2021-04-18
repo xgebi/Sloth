c8c0f67ccf3f8338a24a3c82f586addd5176b288
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-04-18 10:29:53 +0200

    toe:text and toe:content test done, script WIP 🦝

--- test/python/toes/test_xml_parser.py
+++ test/python/toes/test_xml_parser.py
@@ -2,6 +2,8 @@ import unittest
 import os
 
 from app.toes.root_node import RootNode
+from app.toes.comment_node import CommentNode
+from app.toes.node import Node
 from app.toes.xml_parser import XMLParser
 
 
@@ -64,21 +66,31 @@ class MyTestCase(unittest.TestCase):
         xp = XMLParser(path=os.path.join(os.getcwd(), "resources", "toes", "toe_text.toe.html"))
         node: RootNode = xp.parse_file()
 
+        self.assertEqual(len(node.children), 1)
         self.assertEqual(len(node.children[0].children), 3)
+        self.assertEqual(type(node.children[0].children[1]), CommentNode)
+        self.assertEqual(node.children[0].children[1].content, "note strInVar shouldn't have html")
+        self.assertEqual(type(node.children[0].children[0]), Node)
+        self.assertEqual(node.children[0].children[0].attributes["toe:text"], "strInVar")
+        self.assertEqual(type(node.children[0].children[2]), Node)
+        self.assertEqual(node.children[0].children[2].attributes["toe:text"], "'I override but HTML formatting is applied <br />'")
 
     def test_content(self):
         xp = XMLParser(path=os.path.join(os.getcwd(), "resources", "toes", "toe_content.toe.html"))
         node: RootNode = xp.parse_file()
 
+        self.assertEqual(len(node.children), 1)
         self.assertEqual(len(node.children[0].children), 3)
-        # self.assertEqual(len(node.children), 1)
-        # self.assertEqual(node.children[0].get_name(), 'toe:fragment')
-        # self.assertEqual(node.children[0].children[0].paired_tag, True)
-        # self.assertEqual(node.children[0].children[0].get_name(), "a")
-        # self.assertEqual(node.children[0].children[0].attributes["toe:href"], "'I override'")
-        # self.assertEqual(node.children[0].children[0].attributes["href"], "/link-to-nowhere")
-        # self.assertEqual(len(node.children[0].children[0].children), 1)
-        # self.assertEqual(node.children[0].children[0].children[0].content, "Link")
+        self.assertEqual(type(node.children[0].children[1]), CommentNode)
+        self.assertEqual(node.children[0].children[1].content, "note strInVar should have html")
+        self.assertEqual(type(node.children[0].children[0]), Node)
+        self.assertEqual(node.children[0].children[0].attributes["toe:content"], "strInVar")
+        self.assertEqual(type(node.children[0].children[2]), Node)
+        self.assertEqual(node.children[0].children[2].attributes["toe:content"], "'I override <br />'")
+
+    def test_script(self):
+        xp = XMLParser(path=os.path.join(os.getcwd(), "resources", "toes", "script_inline.toe.html"))
+        node: RootNode = xp.parse_file()
 
 
 if __name__ == '__main__':
