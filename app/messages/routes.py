52c5842c7cc549b4f978d7830e30c46a435a2769
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-05-30 17:43:59 +0200

    reading is now better 💟

--- app/messages/routes.py
+++ app/messages/routes.py
@@ -1,6 +1,6 @@
 from flask import abort, redirect, render_template, make_response, request,current_app
 from app.utilities.db_connection import db_connection
-from app.utilities import get_default_language
+from app.utilities import get_default_language, get_languages
 from app.authorization.authorize import authorize_web, authorize_rest
 from app.post.post_types import PostTypes
 from psycopg2 import sql
@@ -9,6 +9,9 @@ from time import time
 import datetime
 import json
 from flask_cors import cross_origin
+from app.toes.hooks import Hooks
+import os
+from app.toes.toes import render_toe_from_path
 
 from app.messages import messages
 
@@ -28,7 +31,7 @@ def show_message_list(*args, permission_level, connection, **kwargs):
     raw_messages = []
     try:
         cur.execute(
-            sql.SQL("""SELECT uuid, name, sent_date, status 
+            sql.SQL("""SELECT uuid, sent_date, status 
             FROM sloth_messages WHERE status != 'deleted' ORDER BY sent_date DESC LIMIT 10""")
         )
         raw_messages = cur.fetchall()
@@ -70,12 +73,16 @@ def show_message(*args, permission_level, connection, msg, **kwargs):
 
     cur = connection.cursor()
 
-    raw_items = []
+    raw_message_fields = []
     try:
         cur.execute(
-            sql.SQL("SELECT sent_date, status FROM sloth_messages WHERE uuid = %s"), [msg]
+            sql.SQL("SELECT sent_date, status FROM sloth_messages WHERE uuid = %s"), (msg, )
         )
         raw_message = cur.fetchone()
+        cur.execute(
+            sql.SQL("SELECT name, value FROM sloth_message_fields WHERE message = %s"), (msg,)
+        )
+        raw_message_fields = cur.fetchall()
         if len(raw_message) > 0:
             cur.execute(sql.SQL("UPDATE sloth_messages SET status = 'read' WHERE uuid = %s"), [msg])
             connection.commit()
@@ -83,15 +90,34 @@ def show_message(*args, permission_level, connection, msg, **kwargs):
         print("db error e")
         abort(500)
 
+    default_lang = get_default_language(connection=connection)
+    languages = get_languages(connection=connection)
+
     cur.close()
     connection.close()
 
     message = {
         "sent_date": datetime.datetime.fromtimestamp(float(raw_message[0])/1000.0).strftime("%Y-%m-%d"),
         "status": raw_message[1].strip(),
+        "items": [{
+            "name": item[0].strip(),
+            "value": item[1].strip()
+        } for item in raw_message_fields]
     }
 
-    return render_template("message.html", post_types=post_types_result, permission_level=permission_level, message=message)
+    return render_toe_from_path(
+        path_to_templates=os.path.join(os.getcwd(), 'app', 'templates'),
+        template="message.toe.html",
+        data={
+            "title": "Message",
+            "post_types": post_types_result,
+            "permission_level": permission_level,
+            "default_lang": default_lang,
+            "languages": languages,
+            "message": message
+        },
+        hooks=Hooks()
+    )
 
 
 @messages.route("/api/messages/delete", methods=["POST", "PUT", "DELETE"])
@@ -135,7 +161,7 @@ def receive_message(*args, connection, **kwargs):
         cur.execute(
             sql.SQL("""INSERT INTO sloth_messages (uuid, sent_date, status) 
             VALUES (%s, %s, %s);"""),
-            (msg_id, time(), "unread")
+            (msg_id, time() * 1000, "unread")
         )
         for key in filled.keys():
             cur.execute(
