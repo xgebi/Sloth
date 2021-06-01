b6cee41ee16a3067a9e0abf4a9f4b41507434aae
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-06-01 19:06:24 +0200

    message improvements 🐌

--- app/messages/routes.py
+++ app/messages/routes.py
@@ -1,4 +1,4 @@
-from flask import abort, redirect, render_template, make_response, request,current_app
+from flask import abort, redirect, render_template, make_response, request, current_app
 from app.utilities.db_connection import db_connection
 from app.utilities import get_default_language, get_languages
 from app.authorization.authorize import authorize_web, authorize_rest
@@ -40,25 +40,30 @@ def show_message_list(*args, permission_level, connection, **kwargs):
         abort(500)
 
     cur.close()
-    default_language = get_default_language(connection=connection)
+    default_lang = get_default_language(connection=connection)
+    languages = get_languages(connection=connection)
     connection.close()
 
-    msgs = []
-    for message in raw_messages:
-        msgs.append({
-            "uuid": message[0],
-            "name": message[1],
-            "sent_date": datetime.datetime.fromtimestamp(float(message[2])/1000.0).strftime("%Y-%m-%d %H:%M"),
-            "status": message[3]
-        })
+    message_list = [{
+        "uuid": message[0],
+        "sent_date": datetime.datetime.fromtimestamp(float(message[1]) / 1000.0).strftime("%Y-%m-%d %H:%M"),
+        "status": message[2]
+    } for message in raw_messages]
 
-    return render_template(
-        "message-list.html",
-        post_types=post_types_result,
-        permission_level=permission_level,
-        messages=msgs,
-        default_lang=default_language
-    )
+
+    return render_toe_from_path(
+        path_to_templates=os.path.join(os.getcwd(), 'app', 'templates'),
+        template="message-list.toe.html",
+        data={
+            "title": "Message",
+            "post_types": post_types_result,
+            "permission_level": permission_level,
+            "default_lang": default_lang,
+            "languages": languages,
+            "messages": message_list
+        },
+        hooks=Hooks()
+)
 
 
 @messages.route("/messages/<msg>")
@@ -76,7 +81,7 @@ def show_message(*args, permission_level, connection, msg, **kwargs):
     raw_message_fields = []
     try:
         cur.execute(
-            sql.SQL("SELECT sent_date, status FROM sloth_messages WHERE uuid = %s"), (msg, )
+            sql.SQL("SELECT sent_date, status FROM sloth_messages WHERE uuid = %s"), (msg,)
         )
         raw_message = cur.fetchone()
         cur.execute(
@@ -97,7 +102,7 @@ def show_message(*args, permission_level, connection, msg, **kwargs):
     connection.close()
 
     message = {
-        "sent_date": datetime.datetime.fromtimestamp(float(raw_message[0])/1000.0).strftime("%Y-%m-%d"),
+        "sent_date": datetime.datetime.fromtimestamp(float(raw_message[0]) / 1000.0).strftime("%Y-%m-%d"),
         "status": raw_message[1].strip(),
         "items": [{
             "name": item[0].strip(),
@@ -166,7 +171,7 @@ def receive_message(*args, connection, **kwargs):
         cur.execute(
             sql.SQL("""SELECT sff.name FROM sloth_form_fields AS sff 
             INNER JOIN sloth_forms sf on sf.uuid = sff.form WHERE sf.name = %s AND sff.is_required = TRUE;"""),
-            (filled["formName"], )
+            (filled["formName"],)
         )
         for required in cur.fetchall():
             if required in filled and len(filled[required]) == 0:
