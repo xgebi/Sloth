17a2b0365c1c08f3b60464791b045670f6d1a4e8
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-08-21 00:31:41 +0200

    Refactored variable scope 🛹

--- rust/toes/src/variable_scope.rs
+++ rust/toes/src/variable_scope.rs
@@ -1,17 +1,16 @@
 use std::collections::HashMap;
-use atree::{Node, Arena};
+use atree::{Node, Arena, Token};
 use std::collections::hash_map::Entry;
 
 struct VariableScope {
     variables: HashMap<String, String>
 }
 
-// TODO rework it to arena and token instead of Node
-fn find_variable(node: &Node<VariableScope>, arena: &Arena<VariableScope>, variable_name: &String) -> Option<String> {
-    if node.data.variables.contains_key(variable_name) {
-        return Some(node.data.variables.get(variable_name).unwrap().clone());
+fn find_variable(arena: &Arena<VariableScope>, token: Token, variable_name: &String) -> Option<String> {
+    if arena.get(token).unwrap().data.variables.contains_key(variable_name) {
+        return Some(arena.get(token).unwrap().data.variables.get(variable_name).unwrap().clone());
     }
-    while let parent = node.ancestors(arena).next().unwrap() {
+    while let parent = arena.get(token).unwrap().ancestors(arena).next().unwrap() {
         if parent.data.variables.contains_key(variable_name) {
             return Some(parent.data.variables.get(variable_name).unwrap().clone());
         }
@@ -19,11 +18,11 @@ fn find_variable(node: &Node<VariableScope>, arena: &Arena<VariableScope>, varia
     return None
 }
 
-fn is_variable(node: &Node<VariableScope>, arena: &Arena<VariableScope>, variable_name: &String) -> bool {
-    if node.data.variables.contains_key(variable_name) {
-        return node.data.variables.contains_key(variable_name);
+fn is_variable(arena: &Arena<VariableScope>, token: Token, variable_name: &String) -> bool {
+    if arena.get(token).unwrap().data.variables.contains_key(variable_name) {
+        return arena.get(token).unwrap().data.variables.contains_key(variable_name);
     } else {
-        while let parent = node.ancestors(arena).next().unwrap() {
+        while let parent = arena.get(token).unwrap().ancestors(arena).next().unwrap() {
             if parent.data.variables.contains_key(variable_name) {
                 return parent.data.variables.contains_key(variable_name);
             }
@@ -32,23 +31,28 @@ fn is_variable(node: &Node<VariableScope>, arena: &Arena<VariableScope>, variabl
     return false;
 }
 
-fn create_variable(node: &mut Node<VariableScope>, arena: &Arena<VariableScope>, variable_name: String, variable_value: String) -> Result<(), ()> {
-    if !node.data.variables.contains_key(&*variable_name) {
-        node.data.variables.insert(variable_name, variable_value);
+fn create_variable(arena: &mut Arena<VariableScope>, token: Token, variable_name: String, variable_value: String) -> Result<(), ()> {
+    if !arena.get(token).unwrap().data.variables.contains_key(&*variable_name) {
+        arena.get_mut(token).unwrap().data.variables.insert(variable_name, variable_value);
         return Ok(());
     }
     Err(())
 }
 
-fn assign_variable(node: &mut Node<VariableScope>, arena: &mut Arena<VariableScope>, variable_name: String, mut variable_value: String) -> Result<(), ()> {
-    if node.data.variables.contains_key(&*variable_name) {
-        node.data.variables.insert(variable_name, variable_value);
+fn assign_variable(arena: &mut Arena<VariableScope>, token: Token, variable_name: String, mut variable_value: String) -> Result<(), ()> {
+    if arena.get(token).unwrap().data.variables.contains_key(&*variable_name) {
+        arena.get_mut(token).unwrap().data.variables.insert(variable_name, variable_value);
         return Ok(());
     } else {
-        while let mut parent = node.ancestors_tokens(arena).next().unwrap() {
-            if arena.get(parent).unwrap().data.variables.contains_key(&*variable_name) {
-                arena.get_mut(parent).unwrap().data.variables.entry(variable_name).and_modify(|e| { *e = variable_value });
-                return Ok(());
+        while let Some(mut parent) = arena.get(token).unwrap().ancestors_tokens(arena).next() {
+            match arena.get_mut(parent) {
+                None => { continue; }
+                Some(parent_variables) => {
+                    if !parent_variables.data.variables.contains_key(&*variable_name) {
+                        parent_variables.data.variables.insert(variable_name.clone(), variable_value.clone());
+                        return Ok(());
+                    }
+                }
             }
         }
     }
@@ -67,7 +71,7 @@ mod tests {
         top_scope.variables.insert("myVar".parse().unwrap(), "'1'".parse().unwrap());
         let (mut arena, root_token) = Arena::with_data(top_scope);
 
-        assert_eq!(is_variable(&arena[root_token], &arena, &String::from("myVar")), true);
+        assert_eq!(is_variable(&arena, root_token, &String::from("myVar")), true);
     }
 
     #[test]
@@ -76,15 +80,26 @@ mod tests {
         top_scope.variables.insert("myVar".parse().unwrap(), "'1'".parse().unwrap());
         let (mut arena, root_token) = Arena::with_data(top_scope);
 
-        assert_eq!(find_variable(&arena[root_token], &arena, &String::from("myVar")), Some(String::from("'1'")));
+        assert_eq!(find_variable(&arena, root_token, &String::from("myVar")), Some(String::from("'1'")));
     }
 
     #[test]
     fn assign_variable_not_in_scope() {
         let mut top_scope = VariableScope { variables: HashMap::new() };
+        let expected = Err(());
+        let (mut arena, root_token) = Arena::with_data(top_scope);
+        let result = assign_variable(&mut arena, root_token, String::from("myVar"), String::from("'1'"));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn assign_variable_in_scope() {
+        let mut top_scope = VariableScope { variables: HashMap::new() };
+        top_scope.variables.insert(String::from("myVar"), String::from("'2'"));
         let expected = Ok(());
         let (mut arena, root_token) = Arena::with_data(top_scope);
-        let result = assign_variable(&mut arena[root_token], &mut arena, String::from("myVar"), String::from("'1'"));
+        let result = assign_variable(&mut arena, root_token, String::from("myVar"), String::from("'1'"));
         assert_eq!(result, expected);
+        assert_eq!(arena.get(root_token).unwrap().data.variables.get("myVar").unwrap().clone(), String::from("'1'"))
     }
 }
