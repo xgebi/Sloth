13b35c2139248c4320343152659553e26c2999ea
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-08-21 09:55:02 +0200

    Checking tag pairedness ⛩

--- rust/toes/src/node.rs
+++ rust/toes/src/node.rs
@@ -1,11 +1,56 @@
+pub(crate) enum NodeTypes {
+    Node,
+    Root,
+    Processing,
+    Directive,
+    Text,
+    Comment,
+    Cdata // look if it is necessary
+
+}
+
 pub struct ToeNode {
-    pub(crate) name: String
+    pub(crate) name: String,
+    pub(crate) node_type: NodeTypes,
+}
+
+impl ToeNode {
+    fn to_string() {
+
+    }
+
+    fn is_unpaired(tag_name: String) -> bool {
+        let unpaired_tags = ["base", "br", "meta", "hr", "img", "track", "source", "embed", "col", "input", "link"];
+        unpaired_tags.contains(&&*tag_name)
+    }
+
+    fn is_always_paired(tag_name: String) -> bool {
+        let paired_tags = ["script"];
+        paired_tags.contains(&&*tag_name)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::ToeNode;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn is_tag_paired() {
+        assert_eq!(ToeNode::is_always_paired(String::from("script")), true);
+    }
+
+    #[test]
+    fn is_not_tag_paired() {
+        assert_eq!(ToeNode::is_always_paired(String::from("br")), false);
+    }
+
+    #[test]
+    fn is_tag_unpaired() {
+        assert_eq!(ToeNode::is_unpaired(String::from("br")), true);
+    }
+
+    #[test]
+    fn is_not_tag_unpaired() {
+        assert_eq!(ToeNode::is_unpaired(String::from("div")), false);
     }
 }
