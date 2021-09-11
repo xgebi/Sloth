307d298bcd2963525fdad145347bf45d32aeba8a
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-09-11 10:10:06 +0200

    Login refactor 🤠

--- app/authorization/user.py
+++ app/authorization/user.py
@@ -1,167 +1,200 @@
-from flask import current_app, make_response
+from flask import current_app, make_response, Response
 import psycopg2
 from psycopg2 import sql
 import bcrypt
 from time import time
 import json
 import uuid
+from typing import Tuple
 
 from app.utilities.db_connection import db_connection
 
+
+class UserInfo:
+    def __init__(self, user_uuid: str, display_name: str, token: str, expiry_time: float, permissions_level: int):
+        self.uuid = user_uuid
+        self.display_name = display_name
+        self.token = token
+        self.expiry_time = expiry_time
+        self.permissions_level = permissions_level
+
+    def to_json_string(self) -> str:
+        return json.dumps({
+            "uuid": self.uuid,
+            "displayName": self.display_name,
+            "token": self.token,
+            "expiryTime": self.expiry_time,
+            "permissionsLevel": self.permissions_level,
+        })
+
+
 class User:
-	def __init__(self, uuid = None, token = None):
-		self.uuid = uuid
-		self.token = token
+    def __init__(self, uuid=None, token=None):
+        self.uuid = uuid
+        self.token = token
 
-	@db_connection
-	def login_user(self, username, password, connection=None):
-		config = current_app.config
-		if connection is None:
-			return None
-		cur = connection.cursor()
+    @db_connection
+    def login_user(self, username: str, password: str, connection=None) -> UserInfo or None:
+        config = current_app.config
+        if connection is None:
+            return None
+        cur = connection.cursor()
 
-		items = []
+        items = []
 
-		try:            
-			cur.execute(
-				sql.SQL("SELECT uuid, password, display_name, permissions_level FROM sloth_users WHERE username = %s"),
-				[username]
-			)
-			items = cur.fetchone()
-		except Exception as e:
-			return None
+        try:
+            cur.execute(
+                sql.SQL("SELECT uuid, password, display_name, permissions_level FROM sloth_users WHERE username = %s"),
+                [username]
+            )
+            items = cur.fetchone()
+        except Exception as e:
+            return None
 
-		if items is None:
-			return None
-		
-		trimmed_items = {
-			"uuid": items[0], 
-			"password": items[1],
-			"display_name": items[2],
-			"permissions_level": items[3]
-			}
-		token = ""
-		
-		if bcrypt.checkpw(password.encode('utf8'), trimmed_items["password"].encode('utf8')):
-			token = str(uuid.uuid4())
-			expiry_time = time() + 1800 # 30 minutes
+        if items is None:
+            return None
 
-			try:            
-				cur.execute(
-					sql.SQL("UPDATE sloth_users SET token = %s, expiry_date = %s WHERE uuid = %s"), (token, expiry_time, trimmed_items["uuid"])
-				)
-				connection.commit()
-			except Exception as e:
-				cur.close()
-				connection.close()
-				return None
-			
-			cur.close()
-			connection.close()
-			return { 
-				"uuid" : trimmed_items["uuid"],
-				"display_name": trimmed_items["display_name"],
-				"token": token,
-				"expiryTime": expiry_time * 1000,
-				"permissionsLevel": trimmed_items["permissions_level"]
-			}
-		
-		cur.close()
-		connection.close()
+        trimmed_items = {
+            "uuid": items[0],
+            "password": items[1],
+            "display_name": items[2],
+            "permissions_level": items[3]
+        }
 
-		return None
+        if bcrypt.checkpw(password.encode('utf8'), trimmed_items["password"].encode('utf8')):
+            token = str(uuid.uuid4())
+            expiry_time = time() + 1800  # 30 minutes
 
+            try:
+                cur.execute(
+                    sql.SQL("UPDATE sloth_users SET token = %s, expiry_date = %s WHERE uuid = %s"),
+                    (token, expiry_time, trimmed_items["uuid"])
+                )
+                connection.commit()
+            except Exception as e:
+                cur.close()
+                connection.close()
+                return None
 
-	def authorize_user(self, permissions_level):
-		config = current_app.config
-		con = psycopg2.connect(dbname=config["DATABASE_NAME"], user=config["DATABASE_USER"], host=config["DATABASE_URL"], port=config["DATABASE_PORT"], password=config["DATABASE_PASSWORD"])
-		cur = con.cursor()
-		
-		try:            
-			cur.execute(
-				sql.SQL("SELECT permissions_level, expiry_date, token FROM sloth_users WHERE uuid = %s"), [self.uuid]
-			)
-			items = cur.fetchone()
-		except Exception as e:
-			cur.close()
-			con.close()            
-			return False
-		
-		if (items is None or permissions_level > items[0] or time() > items[1] or self.token != items[2]):
-			cur.close()
-			con.close()
-			return False
-		
-		try:            
-			cur.execute(
-				sql.SQL("UPDATE sloth_users SET expiry_date = %s WHERE uuid = %s"),
-				(time() + 1800, self.uuid)
-			)
-			con.commit()
-		except Exception as e:
-			cur.close()
-			con.close()
-			return False
+            cur.close()
+            connection.close()
+            return UserInfo(
+                user_uuid=trimmed_items["uuid"],
+                display_name=trimmed_items["display_name"],
+                token=token,
+                expiry_time=expiry_time * 1000,
+                permissions_level=trimmed_items["permissions_level"]
+            )
 
-		cur.close()
-		con.close()
-		return (True, items[0])
+        cur.close()
+        connection.close()
 
-	def logout_user(self):
-		config = current_app.config
-		con = psycopg2.connect(dbname=config["DATABASE_NAME"], user=config["DATABASE_USER"], host=config["DATABASE_URL"], port=config["DATABASE_PORT"], password=config["DATABASE_PASSWORD"])
-		cur = con.cursor()
+        return None
 
-		try:            
-			cur.execute(
-				sql.SQL("UPDATE sloth_users SET expiry_date = %s, token = %s WHERE uuid = %s"), (0, "", self.uuid)
-			)
-			con.commit()
-		except Exception as e:
-			cur.close()
-			con.close()
-			return None
-		cur.close()
-		con.close()
+    def authorize_user(self, permissions_level) -> Tuple[bool, int]:
+        config = current_app.config
+        con = psycopg2.connect(dbname=config["DATABASE_NAME"], user=config["DATABASE_USER"],
+                               host=config["DATABASE_URL"], port=config["DATABASE_PORT"],
+                               password=config["DATABASE_PASSWORD"])
+        cur = con.cursor()
 
-		return False
+        try:
+            cur.execute(
+                sql.SQL("SELECT permissions_level, expiry_date, token FROM sloth_users WHERE uuid = %s"), [self.uuid]
+            )
+            items = cur.fetchone()
+        except Exception as e:
+            cur.close()
+            con.close()
+            return False, -1
 
-	def refresh_login(self):
-		config = current_app.config
-		con = psycopg2.connect(dbname=config["DATABASE_NAME"], user=config["DATABASE_USER"], host=config["DATABASE_URL"], port=config["DATABASE_PORT"], password=config["DATABASE_PASSWORD"])
-		cur = con.cursor()
+        if items is None or permissions_level > items[0] or time() > items[1] or self.token != items[2]:
+            cur.close()
+            con.close()
+            return False, -1
 
-		try:            
-			cur.execute(
-				sql.SQL("SELECT token, expiry_date FROM sloth_users WHERE uuid = %s"), [self.uuid]
-			)
+        try:
+            cur.execute(
+                sql.SQL("UPDATE sloth_users SET expiry_date = %s WHERE uuid = %s"),
+                (time() + 1800, self.uuid)
+            )
+            con.commit()
+        except Exception as e:
+            cur.close()
+            con.close()
+            return False, -1
 
-			item = cur.fetchone()
-			if (item[1] < time()):
-				response = make_response(json.dumps({"error": "Authorization timeout"}))
-				response.headers['Content-Type'] = 'application/json'
-				code = 403
+        cur.close()
+        con.close()
+        return True, int(items[0])
 
-				return response, code
-			
-			if (item[0] != self.token):
-				response = make_response(json.dumps({"Unauthorized": True}))
-				response.headers['Content-Type'] = 'application/json'
-				code = 403
+    def logout_user(self):
+        config = current_app.config
+        con = psycopg2.connect(dbname=config["DATABASE_NAME"], user=config["DATABASE_USER"],
+                               host=config["DATABASE_URL"], port=config["DATABASE_PORT"],
+                               password=config["DATABASE_PASSWORD"])
+        cur = con.cursor()
 
-				return response, code
+        try:
+            cur.execute(
+                sql.SQL("UPDATE sloth_users SET expiry_date = %s, token = %s WHERE uuid = %s"), (0, "", self.uuid)
+            )
+            con.commit()
+        except Exception as e:
+            cur.close()
+            con.close()
+            return
+        cur.close()
+        con.close()
 
-			expiry_time = time() + 1800 # 30 minutes
+    def refresh_login(self) -> Tuple[Response, int]:
+        config = current_app.config
+        con = psycopg2.connect(dbname=config["DATABASE_NAME"], user=config["DATABASE_USER"],
+                               host=config["DATABASE_URL"], port=config["DATABASE_PORT"],
+                               password=config["DATABASE_PASSWORD"])
+        cur = con.cursor()
 
-			cur.execute(
-				sql.SQL("UPDATE sloth_users SET expiry_date = %s WHERE uuid = %s"), (expiry_time, self.uuid)
-			)
-			con.commit()
-		except Exception as e:
-			cur.close()
-			con.close()
-			return None
-		cur.close()
-		con.close()
+        try:
+            cur.execute(
+                sql.SQL("SELECT token, expiry_date FROM sloth_users WHERE uuid = %s"), [self.uuid]
+            )
 
-		return True
+            item = cur.fetchone()
+            if item[1] < time():
+                response = make_response(json.dumps({"error": "Authorization timeout"}))
+                response.headers['Content-Type'] = 'application/json'
+                code = 403
+
+                return response, code
+
+            if item[0] != self.token:
+                response = make_response(json.dumps({"Unauthorized": True}))
+                response.headers['Content-Type'] = 'application/json'
+                code = 403
+
+                return response, code
+
+            expiry_time = time() + 1800  # 30 minutes
+
+            cur.execute(
+                sql.SQL("UPDATE sloth_users SET expiry_date = %s WHERE uuid = %s"), (expiry_time, self.uuid)
+            )
+            con.commit()
+        except Exception as e:
+            cur.close()
+            con.close()
+
+            response = make_response(json.dumps({"Unauthorized": True}))
+            response.headers['Content-Type'] = 'application/json'
+            code = 403
+
+            return response, code
+
+        cur.close()
+        con.close()
+
+        response = make_response(json.dumps({"refreshLogin": True}))
+        response.headers['Content-Type'] = 'application/json'
+        code = 200
+
+        return response, code
--- app/login/routes.py
+++ app/login/routes.py
@@ -1,11 +1,12 @@
-from flask import request, redirect, make_response, render_template
+from flask import request, redirect, make_response
 
-from app.authorization.authorize import authorize_rest
-from app.authorization.user import User
+from app.authorization.authorize import authorize_rest, authorize_web
+from app.authorization.user import User, UserInfo
 from app.toes.toes import render_toe_from_path
 from app.toes.hooks import Hooks
 import json
 import os
+from typing import Tuple
 
 from app.login import login
 
@@ -49,23 +50,22 @@ def process_login(*args, connection=None, **kwargs):
         return redirect("/login/error")
     # compare credentials with database
     user = User()
-    info = user.login_user(username, password)
+    info: UserInfo = user.login_user(username, password)
 
     # if good redirect to dashboard
     if info is not None:
         response = make_response(redirect('/dashboard' if request.args.get("redirect") is None else request.args.get("redirect")))
-        response.set_cookie('sloth_session', info["display_name"] + ":" + info["uuid"] + ":" + info["token"])
+        response.set_cookie('sloth_session', f"{info.display_name}:{info.uuid}:{info.token}")
         return response
     return redirect("/login/error")
 
 
 @login.route("/logout")
-def logout():
+@authorize_web(0)
+def logout(*args, permission_level, **kwargs):
     cookie = request.cookies.get('sloth_session')
-    if cookie is not None and len(cookie.split(":")) == 3:
-        cookie = cookie.split(":")
-        user = User(cookie[1], cookie[2])
-        user.logout_user()
+    user = User(cookie[1], cookie[2])
+    user.logout_user()
 
     response = make_response(redirect('/login'))
     response.set_cookie('sloth_session', "")
@@ -76,3 +76,32 @@ def logout():
 @authorize_rest(0)
 def keep_logged_in(*args, permission_level, **kwargs):
     return json.dumps({"loggedIn": True})
+
+
+@login.route("/api/login")
+def api_login() -> Tuple[str, int]:
+    # get credentials
+    data = json.loads(request.data)
+    username = data.get("username")
+    password = data.get("password")
+    if len(username) == 0 or len(password) == 0:
+        return json.dumps({"error": "name and password can't empty"}), 401
+    # compare credentials with database
+    user = User()
+    info: UserInfo = user.login_user(username, password)
+
+    # if good redirect to dashboard
+    if info is not None:
+        return info.to_json_string(), 200
+    return json.dumps({"error": "Unable to login"}), 401
+
+
+@login.route("/api/logout")
+@authorize_rest(0)
+def api_logout() -> Tuple[str, int]:
+    data = json.loads(request.data)
+    username = data.get("username")
+    token = data.get("token")
+    user = User(username, token)
+    user.logout_user()
+    return json.dumps({"status": "logged out"}), 200
