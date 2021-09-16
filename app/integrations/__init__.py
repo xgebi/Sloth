eafd2b55a5267563527aa80a007f78a6f2e1363a
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-09-16 18:38:59 +0200

    Slowly updating psycopg2 -> psycopg3 🏔

--- app/integrations/__init__.py
+++ app/integrations/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 integrations = Blueprint('integrations', __name__, template_folder = 'templates')
-from app.integrations import integrations
+from app.integrations import routes
