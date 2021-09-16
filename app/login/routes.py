10faf1b8b3df50180ff6d39340907046bce55ea2
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-09-16 23:58:33 +0200

    Throttling logging in 🍰

--- app/login/routes.py
+++ app/login/routes.py
@@ -1,3 +1,7 @@
+import time
+import uuid
+
+import psycopg
 from flask import request, redirect, make_response
 
 from app.authorization.authorize import authorize_rest, authorize_web
@@ -9,6 +13,7 @@ import os
 from typing import Tuple
 
 from app.login import login
+from app.utilities.db_connection import db_connection
 
 
 @login.route("/login")
@@ -42,7 +47,37 @@ def show_login_error():
 
 
 @login.route('/login/process', methods=["POST"])
-def process_login(*args, connection=None, **kwargs):
+@db_connection
+def process_login(*args, connection: psycopg.Connection, **kwargs):
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+
+    with connection.cursor() as cur:
+        cur.execute("""
+        SELECT banned_until, attempts, time_period FROM sloth_ban_list 
+        WHERE ip = %s;""",
+                    (ip, )
+                    )
+        banned_raw = cur.fetchone()
+        if banned_raw is not None:
+            banned = {
+                "ip": ip,
+                "banned_until": banned_raw[0],
+                "attempts": banned_raw[1],
+                "time_period": banned_raw[2],
+            }
+            if banned["banned_until"] > time.time() * 1000:
+                return redirect("/login/error")
+        else:
+            banned = {
+                "ip": ip,
+                "banned_until": time.time() * 1000,
+                "attempts": 0,
+                "time_period": 0,
+            }
+
     # get credentials
     username = request.form.get("username")
     password = request.form.get("password")
@@ -54,9 +89,35 @@ def process_login(*args, connection=None, **kwargs):
 
     # if good redirect to dashboard
     if info is not None:
+        with connection.cursor() as cur:
+            cur.execute("""DELETE FROM sloth_ban_list WHERE ip = %s""", (ip, ))
+            connection.commit()
         response = make_response(redirect('/dashboard' if request.args.get("redirect") is None else request.args.get("redirect")))
         response.set_cookie('sloth_session', f"{info.display_name}:{info.uuid}:{info.token}")
         return response
+    else:
+        banned["attempts"] += 1
+        if banned["attempts"] < 5:
+            banned["time_period"] = 0
+        elif banned["attempts"] < 10:
+            banned["time_period"] = 5 * 60 * 1000
+        else:
+            banned["time_period"] = 24 * 60 * 60 * 1000
+        banned["banned_until"] = (time.time() * 1000) + banned["time_period"]
+        with connection.cursor() as cur:
+            if banned["attempts"] > 1:
+                cur.execute("""UPDATE sloth_ban_list SET attempts = %s, time_period = %s, banned_until = %s,
+                last_attempt = %s 
+                WHERE ip = %s;""",
+                            (banned["attempts"], banned["time_period"], banned["banned_until"], time.time() * 1000,
+                             banned["ip"]))
+            else:
+                cur.execute("""INSERT INTO sloth_ban_list (uuid, ip, attempts, last_attempt, banned_until, time_period) 
+                VALUES (%s, %s, %s, %s, %s, %s)""",
+                            (str(uuid.uuid4()), banned["ip"], banned["attempts"], time.time() * 1000,
+                             banned["banned_until"], banned["time_period"])
+                            )
+            connection.commit()
     return redirect("/login/error")
 
 
