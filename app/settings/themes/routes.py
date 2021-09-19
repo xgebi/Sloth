4e4dd3908ae422be38b0d82618ebb059a2895995
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-09-19 21:57:06 +0200

    Theme list to psycopg3, Rust toes got prioritized higher 😓

--- app/settings/themes/routes.py
+++ app/settings/themes/routes.py
@@ -1,19 +1,17 @@
-from flask import request, flash, url_for, current_app, abort, redirect, render_template
-import psycopg2
-from psycopg2 import sql
-
-import app
+from flask import request, current_app, abort, redirect
+import psycopg
 import os
 from pathlib import Path
-import bcrypt
 import json
 import zipfile
 import shutil
 
-from app.utilities.db_connection import db_connection_legacy
+from app.toes.toes import render_toe_from_path
+from app.utilities.db_connection import db_connection
 from app.utilities import get_default_language
 from app.authorization.authorize import authorize_web, authorize_rest
 from app.post.post_generator import PostGenerator
+from app.toes.hooks import Hooks
 
 from app.post.post_types import PostTypes
 
@@ -22,31 +20,24 @@ from app.settings.themes import settings_themes
 
 @settings_themes.route("/settings/themes")
 @authorize_web(1)
-@db_connection_legacy
-def show_theme_settings(*args, permission_level, connection, **kwargs):
-    if connection is None:
-        return redirect("/database-error")
-    settings = {}
-
+@db_connection
+def show_theme_settings(*args, permission_level: int, connection: psycopg.Connection, **kwargs):
     post_types = PostTypes()
     post_types_result = post_types.get_post_type_list(connection)
-
     config = current_app.config
 
-    cur = connection.cursor()
-
-    active_theme = ""
     try:
-        cur.execute(
-            "SELECT settings_value FROM sloth_settings WHERE settings_name = 'active_theme'"
-        )
-        raw_items = cur.fetchone()
-        active_theme = raw_items[0]
-    except Exception as e:
+        with connection.cursor() as cur:
+            cur.execute(
+                "SELECT settings_value FROM sloth_settings WHERE settings_name = 'active_theme'"
+            )
+            raw_items = cur.fetchone()
+            active_theme = raw_items[0]
+    except Exception:
+        connection.close()
         print("db error")
         abort(500)
 
-    cur.close()
     default_language = get_default_language(connection=connection)
     connection.close()
 
@@ -61,32 +52,46 @@ def show_theme_settings(*args, permission_level, connection, **kwargs):
                 if theme["choosable"] and theme["name"].find(" ") == -1:
                     themes.append(theme)
 
-    return render_template(
-        "theme-list.html",
-        post_types=post_types_result,
-        permission_level=permission_level,
-        themes=themes,
-        active_theme=active_theme,
-        regenerating=Path(os.path.join(os.getcwd(), 'generating.lock')).is_file(),
-        default_lang=default_language
+    return render_toe_from_path(
+        path_to_templates=os.path.join(os.getcwd(), 'app', 'templates'),
+        template="theme-list.toe.html",
+        data={
+            "title": "List of themes",
+            "post_types": post_types_result,
+            "permission_level": permission_level,
+            "default_lang": default_language,
+            "themes": themes,
+            "active_theme": active_theme,
+            "regenerating": Path(os.path.join(os.getcwd(), 'generating.lock')).is_file(),
+        },
+        hooks=Hooks()
     )
 
 
 @settings_themes.route("/settings/themes/activate/<theme_name>")
 @authorize_web(1)
-@db_connection_legacy
-def save_active_theme_settings(*args, theme_name, connection=None, **kwargs):
+@db_connection
+def save_active_theme_settings(*args, theme_name: str, connection: psycopg.Connection, **kwargs):
+    """
+    Sets an active theme
+
+    :param args:
+    :param theme_name:
+    :param connection:
+    :param kwargs:
+    :return:
+    """
     # save theme theme to database
-    cur = connection.cursor()
     try:
-        cur.execute(
-            sql.SQL("UPDATE sloth_settings SET settings_value = %s WHERE settings_name = 'active_theme';"),
-            [theme_name]
-        )
-        connection.commit()
+        with connection.cursor() as cur:
+            cur.execute("UPDATE sloth_settings SET settings_value = %s WHERE settings_name = 'active_theme';",
+                        (theme_name, ))
+            connection.commit()
     except Exception as e:
         print("db error")
+        connection.close()
         abort(500)
+    connection.close()
     # regenerate all post
     posts_gen = PostGenerator()
     posts_gen.run(everything=True)
@@ -97,6 +102,13 @@ def save_active_theme_settings(*args, theme_name, connection=None, **kwargs):
 @settings_themes.route("/api/upload-theme", methods=["POST"])
 @authorize_rest(1)
 def upload_theme(*args, **kwargs):
+    """
+    API endpoint for uploading a theme
+
+    :param args:
+    :param kwargs:
+    :return:
+    """
     theme = request.files["image"]
     if theme.mimetype == 'application/x-zip-compressed' and theme.filename.endswith(".zip"):
         path = os.path.join(current_app.config["THEMES_PATH"], theme.filename[:theme.filename.rfind(".zip")])
@@ -115,26 +127,27 @@ def upload_theme(*args, **kwargs):
 
 @settings_themes.route("/api/themes/list", methods=["GET"])
 @authorize_rest(1)
-@db_connection_legacy
-def show_themes_list(*args, connection=None, **kwargs):
-    if connection is None:
-        abort(500)
-
-    postTypes = PostTypes()
-    postTypesResult = postTypes.get_post_type_list(connection)
-
+@db_connection
+def show_themes_list(*args, connection: psycopg.Connection, **kwargs):
+    """
+    API endpoint that returns JSON with list of themes
+    :param args:
+    :param connection:
+    :param kwargs:
+    :return:
+    """
     config = current_app.config
 
-    listOfDirs = os.listdir(config["THEMES_PATH"])
+    list_of_dirs = os.listdir(config["THEMES_PATH"])
     themes = []
 
-    for folder in listOfDirs:
+    for folder in list_of_dirs:
         theme_file = Path(config["THEMES_PATH"], folder, 'theme.json')
-        if (theme_file.is_file()):
+        if theme_file.is_file():
             with open(theme_file, 'r') as f:
                 theme = json.loads(f.read())
-                if (theme["choosable"]):
+                if theme["choosable"]:
                     themes.append(theme)
 
     connection.close()
-    return json.dumps({"postTypes": postTypesResult, "themes": themes})
+    return json.dumps({"themes": themes})
