8fc277e56e001ad384c21e642aacfade95a0524b
Sarah Gebauer <xgebi@users.noreply.github.com>
2022-07-17 11:49:27 +0200

    Toe skeleton in progress 🍿

--- src/main/scala/com/sloth/templating/toe/Toe.scala
+++ src/main/scala/com/sloth/templating/toe/Toe.scala
@@ -1,5 +1,33 @@
 package com.sloth.templating.toe
 
-class Toe {
+import com.sloth.templating.nodes._
 
+class Toe {
+  def processTree(): Unit = {
+
+  }
+
+  def processSubtree(newTreeParent: Node, templateTreeNode: Node): Node = {
+    null
+  }
+
+  def processConditionalCssClasses(newTreeNode: Node, conditionAttr: String): Unit = {
+
+  }
+
+  def processCssClassCondition(condition: String): String = {
+    null
+  }
+
+  def processConditionalAttributes(newTreeNode: Node, conditionAttr: String, attrName: String): Unit = {
+
+  }
+
+  def processInlineJS(newTree: Node, nodes: List[TextNode]): Unit = {
+
+  }
+
+  def processToeTag(parentElement: Node, element: Node): Unit = {
+
+  }
 }
---
+++ src/test/scala/com/sloth/templating/toes/ToeTests.scala
@@ -0,0 +1,29 @@
+package com.sloth.templating.toes
+
+import org.scalatest.funspec.AnyFunSpec
+
+class ToeTests extends AnyFunSpec{
+  describe("process tree") {
+
+  }
+
+  describe("process sub tree") {
+
+  }
+
+  describe("process conditional CSS classes") {
+
+  }
+
+  describe("process conditional attributes") {
+
+  }
+
+  describe("process inline js") {
+
+  }
+
+  describe("process Toe tag") {
+
+  }
+}
