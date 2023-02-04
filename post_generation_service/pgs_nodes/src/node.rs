3b4dc2a7b259061ca147f281e788edb730c50770
Sarah Gebauer <xgebi@users.noreply.github.com>
2023-02-04 10:44:30 +0100

    Added functions to Node

--- post_generation_service/pgs_nodes/src/node.rs
+++ post_generation_service/pgs_nodes/src/node.rs
@@ -14,4 +14,27 @@ pub struct Node {
     pub node_type: NodeType,
     pub attributes: HashMap<String, String>,
     pub children: Option<Vec<Node>>,
+    pub content: Option<String>,
+}
+
+impl Node {
+    fn is_unpaired(tag_name: String) -> bool {
+        let unpaired_tags = ["base", "br", "meta", "hr", "img", "track", "source", "embed", "col", "input", "link"];
+        unpaired_tags.contains(&&*tag_name)
+    }
+
+    fn is_always_paired(tag_name: String) -> bool {
+        let paired_tags = ["script"];
+        paired_tags.contains(&&*tag_name)
+    }
+
+    pub fn create_node(name: Option<String>) -> Node {
+        Node{
+            name: name.unwrap(),
+            node_type: NodeTypes::Node,
+            attributes: HashMap::new(),
+            content: None,
+            children: Some(Vec::new()),
+        }
+    }
 }
