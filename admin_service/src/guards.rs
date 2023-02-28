552c89dc41537d567aefacaa4620c34d06f0a6bd
Sarah Gebauer <sarah@sarahgebauer.com>
2023-02-28 23:47:48 +0100

    Guard is no longer blocking building 🥜 (amen, hallelujah, peanut butter)

--- admin_service/src/guards.rs
+++ admin_service/src/guards.rs
@@ -1,6 +1,8 @@
 use std::net::ToSocketAddrs;
 use actix_web::guard::{GuardContext, Guard};
 use actix_web::HttpResponse;
+use actix_web::rt::Runtime;
+use actix_web_lab::__reexports::futures_util::{future, SinkExt, TryFutureExt};
 use async_trait::async_trait;
 use sloth_config_lib::get_config;
 use crate::auth_service::auth_service_client::AuthServiceClient;
@@ -8,42 +10,38 @@ use crate::auth_service::LoggedInRequest;
 
 pub struct LoggedInGuard;
 
-#[async_trait]
 impl Guard for LoggedInGuard {
-    async fn check<'a>(&self, ctx: &'a GuardContext<'a>) -> bool {
+    fn check(&self, ctx: &GuardContext<'_>) -> bool {
         let context = ctx.clone();
-        let headers = context.head().headers();
-        let mut token = "";
+        let mut token = "".to_string();
 
-        let x = context.head()
-                .headers()
-                .get("Accept-Version").unwrap().to_str();
-
-        if let Some(t) = headers.get("authorization") {
-            if let Ok(local) = t.to_str() {
-                token = local;
-            }
-        } else if let Some(t) = headers.get("Authorization") {
-            if let Ok(local) = t.to_str() {
-                token = local;
-            }
+        if context.head().headers.contains_key("authorization") {
+                token = context.head().headers.get("authorization").unwrap().to_str().unwrap().to_string();
+        } else if context.head().headers.contains_key("Authorization") {
+                token = context.head().headers.get("Authorization").unwrap().to_str().unwrap().to_string();
         }
+
         if !token.is_empty() {
-            let conf = get_config();
-            if let Ok(config) = conf {
-                let client_details = format!("{}:{}", config.auth_service.url, config.auth_service.port);
-                let client_result = AuthServiceClient::connect(client_details).await;
-                    if let Ok(mut auth_service) = client_result {
-                        let request = tonic::Request::new(LoggedInRequest {
-                            token: token.to_string(),
-                        });
-                        let response = auth_service.is_logged_in(request).await;
-                        if let Ok(r) = response {
-                            return r.into_inner().result;
-                        }
-                    }
-            }
+            return Runtime::new().unwrap().block_on(check_logged_in(token));
         }
         false
     }
+}
+
+async fn check_logged_in(mut token: String) -> bool {
+    let conf = get_config();
+    if let Ok(config) = conf {
+        let client_details = format!("{}:{}", config.auth_service.url, config.auth_service.port);
+        let client_result = AuthServiceClient::connect(client_details).await;
+        if let Ok(mut auth_service) = client_result {
+            let request = tonic::Request::new(LoggedInRequest {
+                token: token.to_string(),
+            });
+            let response = auth_service.is_logged_in(request).await;
+            if let Ok(r) = response {
+                return r.into_inner().result;
+            }
+        }
+    }
+    false
 }
