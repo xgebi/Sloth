0cc9aef7e378d304432b56e2c02e28c275920bc6
xgebi <sarah@sarahgebauer.com>
2023-11-26 12:33:15 +0100

    WIP pre-test functionality of string helper

--- post_generation_service/pgs_toes/src/string_helpers.rs
+++ post_generation_service/pgs_toes/src/string_helpers.rs
@@ -1,15 +1,13 @@
 use std::cell::RefCell;
+use std::ops::Deref;
 use std::rc::Rc;
 use unicode_segmentation::UnicodeSegmentation;
 use crate::variable_scope::VariableScope;
 
 pub fn process_string_with_variables(s: String, quote: &str, vs: Rc<RefCell<VariableScope>>) -> String {
     let chars = s.graphemes(true).collect::<Vec<&str>>();
-    let mut temp = String::new();
     let result = String::new();
 
-    let mut in_quotes = false;
-    let mut quote = "";
     let mut idx = 0;
     let mut temp_buffer = String::new();
     while idx < chars.len() {
@@ -18,8 +16,9 @@ pub fn process_string_with_variables(s: String, quote: &str, vs: Rc<RefCell<Vari
                 let mut jdx = idx + 1;
                 while jdx < chars.len() {
                     if chars[jdx] == "\"" && chars[jdx - 1] != "\\"{
-                        temp_buffer.push_str(chars[idx + 1..jdx].iter().collect());
+                        temp_buffer = format!("{}{}", temp_buffer, chars[idx + 1..jdx].join(""));
                         idx = jdx + 1;
+                        break;
                     }
                 }
             }
@@ -27,28 +26,51 @@ pub fn process_string_with_variables(s: String, quote: &str, vs: Rc<RefCell<Vari
                 let mut jdx = idx + 1;
                 while jdx < chars.len() {
                     if chars[jdx] == "'" && chars[jdx - 1] != "\\"{
-                        temp_buffer.push_str(chars[idx + 1..jdx].iter().collect());
+                        temp_buffer = format!("{}{}", temp_buffer, chars[idx + 1..jdx].join(""));
                         idx = jdx + 1;
+                        break;
                     }
                 }
             }
             _ => {
-                temp_buffer.push_str(chars[idx]);
-                idx += 1;
+                let parsed_number = chars[idx].parse::<i64>();
+                let next_parsed_number = chars[idx + 1].parse::<i64>();
+                if (chars[idx] == "-" && next_parsed_number.is_ok()) || parsed_number.is_ok() {
+                    let mut jdx = idx + 1;
+                    while jdx < chars.len() {
+                        if chars[jdx] == " " {
+                            let num_str = chars[idx + 1..jdx].join("");
+                            let num = num_str.parse::<i64>();
+                            if num.is_ok() {
+                                temp_buffer = format!("{}{} ", temp_buffer, num.unwrap());
+                                idx = jdx + 1;
+                            } else {
+                                panic!("Error parsing condition")
+                            }
+                            break;
+                        }
+                    }
+                } else {
+                    let mut jdx = idx + 1;
+                    while jdx < chars.len() {
+                        if chars[jdx] == " " {
+                            let var_name = chars[idx + 1..jdx].join("");
+                            let l = vs.try_borrow();
+                            match l {
+                                Ok(ref_vs) => {
+                                    let k = ref_vs.deref().clone();
+                                    k.find_variable(String::from(var_name));
+                                }
+                                Err(_) => {
+                                    panic!("Error parsing condition")
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-
-    // let l = vs.try_borrow();
-    // match l {
-    //     Ok(ref_vs) => {
-    //         let k = ref_vs.deref().clone();
-    //         k.find_variable(part.clone());
-    //     }
-    //     Err(_) => {}
-    // }
-
-
 
     result
 }
