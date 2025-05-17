7454fc87891b1629df0df92d9995d4953ae65d00
Sarah Gebauer <sarah@sarahgebauer.com>
2025-05-17 20:28:03 +0300

    Added migration to add thumbnail alt text to a post, made slugs in taxonomy table lower case

---
+++ CMSService/database/setup/051_adding_alt_column_for_thumbnails_in_post.sql
@@ -0,0 +1,3 @@
+ALTER TABLE sloth_posts ADD COLUMN thumbnail_alt text;
+UPDATE sloth_posts SET thumbnail_alt = (SELECT alt FROM sloth_media_alts WHERE sloth_posts.thumbnail = sloth_media_alts.media AND sloth_posts.lang = sloth_media_alts.lang);
+UPDATE sloth_taxonomy SET slug = LOWER(slug);
