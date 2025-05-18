961e77a63dbe3d7e0facd57e7a77299b66308616
Sarah Gebauer <sarah@sarahgebauer.com>
2025-05-18 22:19:26 +0300

    login page - auto login when there's non expired token

--- webapp/app/(authentication)/login/page.tsx
+++ webapp/app/(authentication)/login/page.tsx
@@ -5,10 +5,23 @@ import styles from './login.module.css';
 import {processLogin} from "@/app/(authentication)/login/login";
 import Form from "next/form";
 import {useSearchParams} from "next/navigation";
+import Cookies from 'js-cookie'
 
 export default function LoginPage() {
 	const searchParams = useSearchParams()
 	const error = searchParams.get('error');
+	const rawCookie = Cookies.get('sloth-admin-token');
+	console.log(rawCookie);
+	if (rawCookie) {
+		console.log('abc')
+		const cookie = JSON.parse(rawCookie);
+		console.log('def', cookie, cookie.uuid, (cookie.expiryTime * 1000), (new Date()).getTime())
+		if (cookie.uuid && (cookie.expiryTime * 1000) > (new Date()).getTime()) {
+			// there might be a better way, good enough for now, it after 10pm
+			window.location.replace(`${window.location.origin}/dashboard`);
+		}
+	}
+
 	return (
 		<main className={styles['login-wrapper']}>
 			<section className={styles['login-section']}>
